5dcbf1d0c1790f73841993f8b3bfb88b3f703071
Anup Kumar <anup.raj1989@gmail.com>
2023-03-18 19:39:51 +0530

    feat(json): add consumer to receive json data

--- src/main/java/com/knowledgetree/kafka/KafkaConsumer.java
+++ src/main/java/com/knowledgetree/kafka/KafkaConsumer.java
@@ -1,7 +1,9 @@
 package com.knowledgetree.kafka;
 
+import static com.knowledgetree.KafkaConstant.KAFKA_JSON_TOPIC;
 import static com.knowledgetree.KafkaConstant.KAFKA_TOPIC;
 
+import com.knowledgetree.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,5 +17,10 @@ public class KafkaConsumer {
   public String consume(String message) {
     LOG.info("Message Received: {}",message);
     return message;
+  }
+  @KafkaListener(topics = KAFKA_JSON_TOPIC, groupId = "kafka-consumer-group")
+  public User consume(User user) {
+    LOG.info("User record Received: {}",user);
+    return user;
   }
 }
